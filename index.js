f287bc0f8a05ba1a1cdbd073a42db104c1e8afda
benb116 <benb116@gmail.com>
2017-07-16 11:01:39 -0700

    Last FM integration!
    It works, scrobbles if 75% of song is played

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var SpotifyWebApi = require('spotify-web-api-node');
+var LastfmAPI = require('lastfmapi');
 var path        = require('path');
 var express     = require('express');
 var compression = require('compression');
@@ -38,14 +39,42 @@ app.get('/login', function(req, res) {
 var authCode = '';
 var access_token = '';
 var refresh_token = '';
+var latestSpotID = '';
+var lastDuration = 0;
+var lastartist = '';
+var lasttrack = '';
+var lastalbum = '';
+var lastthresh = 12;
+
+var lastReady = false;
 
 app.get('/callback/', function(req, res) {
-  console.log(req.query.code);
+  // console.log(req.query.code);
   var authCode = req.query.code
   GetAccessToken(authCode);
   return res.send('good');
 });
 
+var lfm = new LastfmAPI({
+  'api_key' : config.LASTKEY,
+  'secret' : config.LASTSEC
+});
+
+var lastAuthUrl = lfm.getAuthenticationUrl({ 'cb' : 'http://localhost:5000/lastcall/' });
+console.log(lastAuthUrl)
+
+app.get('/lastcall/', function(req, res) {
+  var token = req.query.token;
+  lfm.authenticate(token, function (err, session) {
+    if (err) { throw err; }
+    lastReady = true;
+    config.LASTSES = session.THE_USER_SESSION_KEY;
+    console.log(session); // {"name": "LASTFM_USERNAME", "key": "THE_USER_SESSION_KEY"}
+  });
+  return res.send('last-good');
+});
+
+lfm.setSessionCredentials(config.LASTUSR, config.LASTSES);
 
 /* Set the credentials given on Spotify's My Applications page.
  * https://developer.spotify.com/my-applications
@@ -61,23 +90,75 @@ var spotifyApi = new SpotifyWebApi({
 function GetAccessToken(authCode) {
   spotifyApi.authorizationCodeGrant(authCode)
     .then(function(data) {
-      console.log('Retrieved access token', data.body['access_token']);
+      // console.log('Retrieved access token', data.body['access_token']);
 
       // Set the access token
       spotifyApi.setAccessToken(data.body['access_token']);
+      spotifyApi.setRefreshToken(data.body['refresh_token']);
 
-      // Use the access token to retrieve information about the user connected to it
-      return spotifyApi.getMyCurrentPlaybackState();
-    })
-    .then(function(data) {
-      console.log(data)
-
-      if (data.body.is_playing && data.body.device.name === 'Ben\'s Echo Dot') {
-        console.log('Scrobble')
-        console.log(data.body.item.name)
-      }
+      // Save the amount of seconds until the access token expired
+      tokenExpirationEpoch = (new Date().getTime() / 1000) + data.body['expires_in'];
+      console.log('Retrieved token. It expires in ' + Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
+     
     })
     .catch(function(err) {
       console.log('Something went wrong', err.message);
    });
 }
+
+setInterval(function() {
+  if (!config.LASTSES) {
+    console.log('yes')
+  }
+  if (spotifyApi.getAccessToken()) {
+    spotifyApi.getMyCurrentPlaybackState()
+    .then(function(data) {
+
+      if (data.body.is_playing && data.body.device.name === 'Ben\'s Echo Dot') {
+        if (data.body.item.uri != latestSpotID) {
+          latestSpotID = data.body.item.uri;
+          if (lastDuration > lastthresh) {
+            lfm.track.scrobble({
+              'artist' : lastartist,
+              'album' : lastalbum,
+              'track' : lasttrack,
+              'timestamp' : Math.floor((new Date()).getTime() / 1000)
+            }, function (err, scrobbles) {
+              if (err) { return console.log('We\'re in trouble', err); }
+              console.log('We have just scrobbled:', scrobbles);
+            });
+          }
+
+          lastthresh = data.body.item.duration_ms / 5000 * 0.75;
+
+          lfm.track.updateNowPlaying({
+            'artist' : data.body.item.artists[0].name,
+            'album' : data.body.item.album.name,
+            'track' : data.body.item.name,
+            'timestamp' : Math.floor((new Date()).getTime() / 1000)
+          }, function (err, scrobbles) {
+            if (err) { return console.log('We\'re in trouble', err); }
+            console.log('We have just updated now playing:', scrobbles);
+          });
+
+          lastDuration = 0;
+          lastartist = data.body.item.artists[0].name;
+          lastalbum = data.body.item.album.name;
+          lasttrack = data.body.item.name;
+
+        } else {
+          lastDuration++;
+        }
+      }
+    })
+  } else {
+    spotifyApi.refreshAccessToken()
+      .then(function(data) {
+        tokenExpirationEpoch = (new Date().getTime() / 1000) + data.body['expires_in'];
+        console.log('Refreshed token. It now expires in ' + Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
+      }, function(err) {
+        console.log('Could not refresh the token!', err.message);
+      });
+    console.log('no token')
+  }
+}, 5000);
